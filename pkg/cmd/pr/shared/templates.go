861660bd72e57d3620f6a1a7387caa8cd965ed3b
vilmibm <vilmibm@github.com>
2022-10-17 17:23:47 -0700

    WIP use Prompter in template manager

--- pkg/cmd/pr/shared/templates.go
+++ pkg/cmd/pr/shared/templates.go
@@ -5,13 +5,11 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/AlecAivazis/survey/v2"
 	"github.com/cli/cli/v2/api"
 	"github.com/cli/cli/v2/git"
 	fd "github.com/cli/cli/v2/internal/featuredetection"
 	"github.com/cli/cli/v2/internal/ghrepo"
 	"github.com/cli/cli/v2/pkg/githubtemplate"
-	"github.com/cli/cli/v2/pkg/prompt"
 	"github.com/shurcooL/githubv4"
 )
 
@@ -111,6 +109,10 @@ type Template interface {
 	Body() []byte
 }
 
+type iprompter interface {
+	Select(string, string, []string) (int, error)
+}
+
 type templateManager struct {
 	repo       ghrepo.Interface
 	rootDir    string
@@ -118,6 +120,7 @@ type templateManager struct {
 	isPR       bool
 	httpClient *http.Client
 	detector   fd.Detector
+	prompter   iprompter
 
 	templates      []Template
 	legacyTemplate Template
@@ -126,7 +129,7 @@ type templateManager struct {
 	fetchError error
 }
 
-func NewTemplateManager(httpClient *http.Client, repo ghrepo.Interface, dir string, allowFS bool, isPR bool) *templateManager {
+func NewTemplateManager(httpClient *http.Client, repo ghrepo.Interface, p iprompter, dir string, allowFS bool, isPR bool) *templateManager {
 	cachedClient := api.NewCachedHTTPClient(httpClient, time.Hour*24)
 	return &templateManager{
 		repo:       repo,
@@ -134,6 +137,7 @@ func NewTemplateManager(httpClient *http.Client, repo ghrepo.Interface, dir stri
 		allowFS:    allowFS,
 		isPR:       isPR,
 		httpClient: httpClient,
+		prompter:   p,
 		detector:   fd.NewDetector(cachedClient, repo.RepoHost()),
 	}
 }
@@ -183,12 +187,7 @@ func (m *templateManager) Choose() (Template, error) {
 		blankOption = "Open a blank pull request"
 	}
 
-	var selectedOption int
-	//nolint:staticcheck // SA1019: prompt.SurveyAskOne is deprecated: use Prompter
-	err := prompt.SurveyAskOne(&survey.Select{
-		Message: "Choose a template",
-		Options: append(names, blankOption),
-	}, &selectedOption)
+	selectedOption, err := m.prompter.Select("Choose a template", "", append(names, blankOption))
 	if err != nil {
 		return nil, fmt.Errorf("could not prompt: %w", err)
 	}
